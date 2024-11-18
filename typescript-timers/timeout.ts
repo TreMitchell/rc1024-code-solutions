30554665ddfc999fe02154c67a686ae12de74d5b
Tré Mitchell <128627099+TreMitchell@users.noreply.github.com>
2024-11-18 13:49:04 -0800

    Corrected the use of setTimeout in timeout.ts

--- typescript-timers/timeout.ts
+++ typescript-timers/timeout.ts
@@ -1,6 +1,9 @@
-setTimeout(() => {
-  const $message = document.querySelector('.message');
-  if ($message) {
-    $message.textContent = 'Hello There';
-  }
-}, 2000);
+const $heading = document.querySelector(".message");
+
+function helloThere(): void {
+  if (!$heading) throw new Error("$heading is null");
+
+  $heading.textContent = "Hello There";
+}
+const timeoutId = setTimeout(helloThere, 2000);
+
