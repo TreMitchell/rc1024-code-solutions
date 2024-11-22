327a7c47744ff080b181f90041d43ee01bcb4434
Tré Mitchell <128627099+TreMitchell@users.noreply.github.com>
2024-11-22 09:33:21 -0800

    Updated pokemon variable name in main.ts

--- fetch/main.js
+++ fetch/main.js
@@ -1,27 +1,29 @@
-'use strict';
+"use strict";
 async function fetchUser() {
-  try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users');
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const bulbasaur = (await response.json());
+        console.log(bulbasaur);
     }
-    const pokemon = await response.json();
-    console.log(pokemon);
-  } catch (err) {
-    console.error('Error:', err);
-  }
+    catch (err) {
+        console.error('Error:', err);
+    }
 }
 fetchUser();
 async function fetchPokemon() {
-  try {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/1');
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const bulbasaur = (await response.json());
+        console.log(bulbasaur);
     }
-    const pokemon = await response.json();
-    console.log(pokemon);
-  } catch (err) {
-    console.error('Error:', err);
-  }
+    catch (err) {
+        console.error('Error:', err);
+    }
 }
 fetchPokemon();
