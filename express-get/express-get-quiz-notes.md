2c7a536d75df4349ef24173363a74dcc91738197
TreMitchell <trem963@gmail.com>
2025-01-15 17:58:27 +0000

    Completed express-get exercise.

--- express-get/express-get-quiz-notes.md
+++ express-get/express-get-quiz-notes.md
@@ -5,9 +5,9 @@
 Answer the following questions in the provided markdown file before turning in this exercise:
 
 - What is the purpose of the HTTP `GET` method?
-  .
+  The purpose of the HTTP `GET` method is to get the data of whatever you are trying to select.
 - What is the appropriate `Content-Type` header for HTTP messages that contain JSON in their bodies?
-  .
+  The appropriate `Content-Type` header for HTTP messages that contain JSON in their bodies is `application/json`.
 
 ## Notes
 
--- express-get/server.ts
+++ express-get/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import pg from 'pg';
-import { errorMiddleware } from './lib/index.js';
+import { ClientError, errorMiddleware } from './lib/index.js';
 
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/pagila',
@@ -12,25 +12,44 @@ const db = new pg.Pool({
 
 const app = express();
 
-// app.get('/api/countries', async (req, res, next) => {
-//   console.log('countries hit');
-//   // try {
-//   //   const sql = `select
-//   //     "", "", ""
-//   //     from ""
-//   //     group by`
-//   // } catch (err) {
-//   //   next(err);
-//   // }
-// });
+app.get('/api/countries', async (req, res, next) => {
+  console.log('countries hit');
+  try {
+    const sql = `
+    select "countries"."countryId", "countries"."name", count(*) as "cities"
+    from "countries"
+    join "cities" using ("countryId")
+    group by "countries"."countryId", "countries"."name";
+    `;
+    const result = await db.query(sql);
+    res.json(result.rows);
+  } catch (err) {
+    next(err);
+  }
+});
 
-// app.get('/api/cities/:cityId', async (req, res, next) => {
-//   console.log('countries hit');
-//   try {
-//   } catch (err) {
-//     next(err);
-//   }
-// });
+app.get('/api/cities/:cityId', async (req, res, next) => {
+  console.log('cityId hit');
+  try {
+    const { cityId } = req.params;
+    if (!Number.isInteger(+cityId)) {
+      throw new ClientError(400, `Non-integer cityId: ${cityId}`);
+    }
+    const sql = `
+    select "cities"."cityId", "cities"."name", "countries"."name" as "country"
+    from "cities"
+    join "countries" using ("countryId")
+    where "cityId" = $1;
+    `;
+    const params = [cityId];
+    const result = await db.query(sql, params);
+    const city = result.rows[0];
+    if (!city) throw new ClientError(404, `city ${cityId} not found`);
+    res.json(city);
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.use(errorMiddleware);
 
